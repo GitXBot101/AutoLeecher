91bb7b88ad3eab016abbecde3ec5a631cc055ff6
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2022-11-08 11:56:05 +0530

    Create conf.py

---
+++ apdbot/conf.py
@@ -0,0 +1,9 @@
+from apdbot.sample_con import Config
+#Fill your all data here Do not Edit other Files
+
+class Config(Config):
+    BOT_TOKEN= "2028264142:AAECmBu2SrOd9d342BvRJV1pK4v2mTY0Dtc"
+    APP_ID = 6951121
+    API_HASH = "10654d2aa0d67e2f8759ba6a1644fa65"
+    OWNER_ID = "1482769753"
+    AUTH = [-1001740473527]
